84af8887be8540f473a05ddc49124547a9e3d11b
Amitesh Sharma <amitesh863@gmail.com>
2020-02-06 17:06:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 setup(
   name = 'nbeats_forecast',        
   packages = ['nbeats_forecast'],   
-  version = '1.3.2',     
+  version = '1.3.3',     
   license='MIT',
   description="This library uses nbeats-pytorch as base and simplifies the task of univariate time series forecasting using N-BEATS by providing a interface similar to scikit-learn and keras."
   long_description=long_description,
@@ -15,13 +15,13 @@ setup(
   author = 'Amitesh Sharma',                   
   author_email = 'amitesh863@gmail.com',     
   url = 'https://github.com/amitesh863/nbeats_forecast',   
-  download_url = 'https://github.com/amitesh863/nbeats_forecast/archive/1.3.2.tar.gz',    
+  download_url = 'https://github.com/amitesh863/nbeats_forecast/archive/1.3.3.tar.gz',    
   keywords = ['nbeats', 'timeseries', 'forecast', 'nueral beats' , 'univariate timeseries forecast ', 'timeseries forecast', 'univariate timeseries forecast'],   
   install_requires=[
           'nbeats-pytorch',
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',          
+    'Development Status :: 5 - Production/Stable',         
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
   ],
